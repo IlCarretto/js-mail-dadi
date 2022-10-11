565f832990574d60772bacfa6ac47ca2300a6543
IlCarretto <carlorebuffat412@gmail.com>
2022-10-11 17:41:57 +0200

    mail exercise done

---
+++ mail/js/script.js
@@ -0,0 +1,32 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+// Chiedere all'utente la sua mail
+const userEmail = prompt("Qual è la tua email?");
+console.log(userEmail);
+
+const availableEmails = ["lorem@gmail.com", "ipsum@libero.it", "dolor@hotmail.com", "pincopallino@live.it", "booleanuser@gmail.com"];
+
+let emailResult = false;
+
+for (let i = 0; i < availableEmails.length; i++) {
+    const currentEmail = availableEmails[i];
+    console.log(currentEmail);
+
+    // SE l'email inserita dall'utente è presente nella array
+    if (currentEmail === userEmail) {
+        emailResult = true;
+    }
+}
+
+if (emailResult) {
+    alert("Login valido!")
+} else {
+    alert("Login fallito, riprova!")
+}
